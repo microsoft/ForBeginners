b457b93b238d9107521b46faa4722dac1a86a655
Nitya Narasimhan <nitya@users.noreply.github.com>
2025-01-15 02:53:20 -0500

    update 01 / add devcontainer support

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+
+# Install NPM modules if required for web UI
+echo "Installing NPM modules here"
+# pushd ./src/web
+# npm install
+# npx next telemetry disable
+# popd
